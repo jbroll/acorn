62eee93e783b57880390f99100a2a926f476bcde
John Roll <john@rkroll.com>
2013-03-04 19:50:18 -0500

    Start display, fix non-sequential

--- acorn-model.tcl
+++ acorn-model.tcl
@@ -14,32 +14,33 @@
 
 
 oo::class create ::acorn::Model {
-    variable type current surfaces default basedef basemap
-    accessor type current surfaces default
+    variable grouptype current surfaces default basedef basemap anonsurf
+    accessor grouptype current surfaces default
 
     constructor { args } {
 	procs surface coordinate-break surface-group surface-group-non-sequential
 
-	set type sequential
+	set grouptype sequential
 	set basedef { type simple n 1.00 }
 	set default {}
-	set basemap { type type R R K K n n thickness thickness aper_type aper_type aper_param aper_param x x y y z z rx rx ry ry rz rz }
+	set basemap { type type R R K K n n thickness thickness aperture aperture aper_type aper_type aper_param aper_param aper_min aper_min aper_max aper_max x x y y z z rx rx ry ry rz rz }
+	set anonsurf 0
 
 
 	set body [lindex $args end]
-	set args  [lrange $args 0 end-1]
+	set args [lrange $args 0 end-1]
 
-	foreach { name value } [dict merge { type sequential } $args] { set $name $value }
+	foreach { name value } [dict merge { grouptype sequential } $args] { set $name $value }
 
 	set current [::acorn::Surfs create [namespace current]::surfs[incr [namespace current]::SURFS] 0]
 
 	eval $body
 
-	if { [$current length] } { lappend surfaces $type $current }
+	if { [$current length] } { lappend surfaces $grouptype $current }
 
 	set current Surface-Add-Error
     }
-    method coordinate-break { name args } { surface name [list {*}[join $args] type coordbk] }
+    method coordinate-break { name args } { surface name [list {*}[join $args] grouptype coordbk] }
 
 
     method surfset1 { obj surf parmap cmd args } {
@@ -75,34 +76,52 @@ oo::class create ::acorn::Model {
 	$current $i set {*}[dict merge $basedef $default [mappair $parmap [join $args]] [list name $name]]
 	$current $i set aperture [::acorn::Aperture [$current get aper_type] [$current get aper_param]]
 
-	if { $name ne "." } {
-	    ::oo::objdefine [self] [list forward $name [self] surfset1 $current $i $parmap]
-	    ::oo::objdefine [self] [list export $name]
+	if { $name eq "." } {
+	    set name anon[incr anonsurf]
+	    $current $i set name $name
 	}
+
+
+	::oo::objdefine [self] [list forward $name [self] surfset1 $current $i $parmap]
+	::oo::objdefine [self] [list export $name]
     }
     method surface-group { name args } {
 	set savedefault $default
-	set savetype    $type
+	set savetype    $grouptype
 
 	if { [$current length] } {
-	    lappend surfaces $type $current
+	    lappend surfaces $grouptype $current
 	    set current [::acorn::Surfs create [namespace current]::surfs[incr [namespace current]::SURFS] 0]
 	}
 
 	set body [lindex $args end]
 	set args  [lrange $args 0 end-1]
 
-	foreach { name value } [dict merge { type sequential } $args] { set $name $value }
+	foreach { name value } [dict merge { grouptype sequential } $args] { set $name $value }
 
 	eval $body
 
-	if { [$current length] } { lappend surfaces $type $current }
+	if { [$current length] } { lappend surfaces $grouptype $current }
 	set current [::acorn::Surfs create [namespace current]::surfs[incr [namespace current]::SURFS] 0]
 
 	set default $savedefault
-	set type    $savetype
+	set grouptype    $savetype
     }
-    method surface-group-non-sequential { name args } { tailcall surface-group $name type non-sequential {*}$args }
+    method surface-group-non-sequential { name args } { tailcall surface-group $name grouptype non-sequential {*}$args }
+
+    method foreach-surface { value script } {
+	set i 0
+
+	foreach { type surf } $surfaces {
+	    #slist $i set surf [$surf getptr] nsurf [$surf length] type [string equal $type non-sequential]
+
+	    foreach j [iota 0 [$surf length]-1] {
+		uplevel [list set $value [$surf $j get name]]
+		uplevel $script
+	    }
+	    incr i
+	}
+    }
 
     method trace { rays } {
 	::acorn::SurfaceList create slist 0
@@ -119,6 +138,7 @@ oo::class create ::acorn::Model {
 		    $surf $j set aper_data [$aper getptr]
 		    $surf $j set aper_leng [$aper length]
 		}
+
 	    }
 	    incr i
 	}
--- tripack/tripack.tcl
+++ tripack/tripack.tcl
@@ -1,58 +1,106 @@
+#!/Users/john/bin/tclkit8.6
+#
 
-critcl:ccode { 
-    extern trmesh_(int *nrows, float *x, float *y, int *list, int *lptr, int *lend, int *lnew, int i*er);
-    extern trlist_(int *ncc, int *lcc, int *n, int *list, int *lptr, int *lend, int *nrow, int *nt, int *ltri, int *lct, int *ier);
+#critcl::cheaders -I/Users/john/include -I/home/john/include
+
+#critcl::tcl 8.6
+critcl::clibraries tripack.o
+
+package provide tripack 1.0
+
+critcl::ccode { 
+    #include <stdlib.h>
+
+    extern void trmesh_(int *nrows, float *x, float *y, int *list, int *lptr, int *lend, int *lnew, int *ier);
+    extern void trlist_(int *ncc, int *lcc, int *n, int *list, int *lptr, int *lend, int *nrow, int *nt, int *ltri, int *lct, int *ier);
 }
 
-cproc trimesh { Tcl_Interp* ip Tcl_Obj* points } ok {
-    float *x 
-    float *y
-    float *z
+critcl::cproc trimesh { Tcl_Interp* ip Tcl_Obj* points } ok {
+    int    n;
+    float *x;
+    float *y;
+    float *z;
 
-    int *list = malloc(sizeof(int) * n * 6);
-    int *lptr = malloc(sizeof(int) * n * 6);
-    int *lend = malloc(sizeof(int) * n *  );
+    Tcl_Obj **plist;
+    Tcl_Obj **items;
+    int      nitem;
+
+    Tcl_ListObjGetElements(ip, points, &n, &plist);
+
+    int *list = malloc(sizeof(int) * n *  6);
+    int *lptr = malloc(sizeof(int) * n *  6);
+    int *lend = malloc(sizeof(int) * n);
+    int *ltri = malloc(sizeof(int) * n * 12);
     int lnew = 0;
     int ier  = 0;
 
     int ncc  = 0;
     int lcc  = 0;
-    int ltc  = 0;
-    int nrow = 6
+    int lct  = 0;
+    int nrow = 6;
+
+    x = malloc(sizeof(float) * n);
+    y = malloc(sizeof(float) * n);
+    z = malloc(sizeof(float) * n);
 
     int nt   = 0;;
+    int i, j;
 
-    TclGetListFromObj(ip, points, &plist, &n);
+    Tcl_Obj *result = Tcl_GetObjResult(ip);
+
 
     for ( i = 0; i < n; i++ ) {	
-	TclGetListFromObj(objv[0], &nitem, &items);
+	double val;
 
-	x[i] = TclGetDoubleFromObj(items[0]);
-	y[i] = TclGetDoubleFromObj(items[1]);
-	z[i] = TclGetDoubleFromObj(items[2]);
-    }
-    
+	Tcl_ListObjGetElements(ip, plist[i], &nitem, &items);
 
-    trmesh_(&n, x, y, list, lptr, lend, &lnew, &ier);
-    trlist_(&ncc, &lcc, &n, list, lptr, lend, &nrow, &nt, ltri, &lct, &ier);
-
-    Tcl_Obj *reply = Tcl_GetResultObj(ip);
-
-    for ( i = 0; i < nt; i++ ) {
-	Tcl_Obj *tri = Tcl_NewListObj();
-
-	for ( j = 0; i < 3; j++ ) {
-	    Tcl_AppendListObj(tri, Tcl_NewDoubleObj(x[ltri[i*6+j])
-		    		 , Tcl_NewDoubleObj(y[ltri[i*6+j])
-				 , Tcl_NewDoubleObj(z[ltri[i*6+j]));
+	if ( nitem != 3 ) {
+	    Tcl_AppendStringsToObj(result, "point does not have 3 coords", NULL);
+	    return TCL_ERROR;
 	}
 
-	Tcl_AppendListObj(reply, tri);
+	if ( Tcl_GetDoubleFromObj(ip, items[0], &val) != TCL_OK ) { return TCL_ERROR; };  x[i] = val;
+	if ( Tcl_GetDoubleFromObj(ip, items[1], &val) != TCL_OK ) { return TCL_ERROR; };  y[i] = val;
+	if ( Tcl_GetDoubleFromObj(ip, items[2], &val) != TCL_OK ) { return TCL_ERROR; };  z[i] = val;
+    }
+    
+    ier = 0;
+    trmesh_(&n, x, y, list, lptr, lend, &lnew, &ier);
+
+    if ( ier != 0 ) {
+	Tcl_AppendStringsToObj(result, "cannot mesh", NULL);
+	return TCL_ERROR;
+    }
+
+    ier = 0;
+    trlist_(&ncc, &lcc, &n, list, lptr, lend, &nrow, &nt, ltri, &lct, &ier);
+
+    if ( ier != 0 ) {
+	Tcl_AppendStringsToObj(result, "cannot list", NULL);
+	return TCL_ERROR;
+    }
+
+
+    for ( i = 0; i < nt; i++ ) {
+	Tcl_Obj *tri = Tcl_NewObj();
+
+	for ( j = 0; j < 3; j++ ) {
+	    Tcl_ListObjAppendElement(ip, tri, Tcl_NewDoubleObj(x[ltri[i*6+j]-1]));
+	    Tcl_ListObjAppendElement(ip, tri, Tcl_NewDoubleObj(y[ltri[i*6+j]-1]));
+	    Tcl_ListObjAppendElement(ip, tri, Tcl_NewDoubleObj(z[ltri[i*6+j]-1]));
+	}
+
+	Tcl_ListObjAppendElement(ip, result, tri);
     }
 
     free(list);
     free(lptr);
     free(lend);
+    free(ltri);
+
+    free(x);
+    free(y);
+    free(z);
 
     return TCL_OK;
 }
