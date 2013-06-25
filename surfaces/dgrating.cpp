694c43bc0c16dcd19ea20d6dc040f55c8d85477e
John Roll <john@rkroll.com>
2013-06-25 12:41:52 -0400

    dgrating + bino start

--- surfaces/dgrating.cpp
+++ surfaces/dgrating.cpp
@@ -15,14 +15,6 @@ using namespace Eigen;
 
 extern "C" {
 
-    void prays(Ray *ray, int n)
-    {
-	for ( int i = 0; i < n; i++ ) {
-	    printf("%5d\t%10.6f\t%10.6f\t%10.6f\t", i, ray[i].p(X), ray[i].p(Y), ray[i].p(Z));
-	    printf("%10.6f\t%10.6f\t%10.6f\t%d\n",     ray[i].k(X), ray[i].k(Y), ray[i].k(Z), ray[i].vignetted);
-	}
-    }
-
   static const char  *MyParamNames[] = { "R", "K", "l/mm", "order" };
   static const double MyParamValue[] = { 0.0, 0.0, 0.0, 1.0 };
 
