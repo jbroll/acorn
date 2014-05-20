a8d93cdb2369dea0a497507a9b23985d8f543035
john@cfa.harvard.edu <john@cfa.harvard.edu>
2014-05-20 11:23:53 -0400

    arec worksing + testing zernike to 104

--- zernike/zcmp.c
+++ zernike/zcmp.c
@@ -6,11 +6,12 @@
 
 double drand48();
 
+#define NTERMS 104
 
 int main() {
-	int i;
-	int n = 104;
-	double c[50];
+	int i, z;
+	int n = NTERMS;
+	double c[NTERMS];
 
 	double x;
 	double y;
@@ -24,7 +25,7 @@ int main() {
 
 
 
-    for ( int z = 0; z < n; z++ ) {
+    for ( z = 0; z < n; z++ ) {
 
         for ( i = 0; i < n; i++ ) {
 	    if ( i == z ) {
@@ -39,22 +40,18 @@ int main() {
 	    x = drand48()*0.707;
 	    y = drand48()*0.707;
 
-	    //zernike_std( x,  y, n, c, &c_z, &c_dx, &c_dy);
-	    
+	    zernike_std( x,  y, n, c, &c_z, &c_dx, &c_dy);
+
 	    zernikf_std( x,  y, n, c, &f_z, &f_dx, &f_dy);
-
 	    zernikx_std( x,  y, n, c, &x_z, &x_dx, &x_dy);
-
 	    zernikz_std( x,  y, n, c, &z_z, &z_dx, &z_dy);
 
-	    printf("%d	: %5.3f, %5.3f", z, x, y);
+	    printf("%d	: %5.3f, %5.3f", z+1, x, y);
 	    printf(" : %10.5f %10.5f %10.5f", f_z, f_dx, f_dy);	
 	    printf(" : %10.5f %10.5f %10.5f", x_z-f_z, x_dx-f_dx, x_dy-f_dy);	
 	    printf(" : %10.5f %10.5f %10.5f", z_z-f_z, z_dx-f_dx, z_dy-f_dy);	
-	    //printf(" : %10.5f %10.5f %10.5f", c_z, c_dx, c_dy);	
+	    printf(" : %10.5f %10.5f %10.5f", c_z-f_z, c_dx-f_dx, c_dy-f_dy);	
 	    printf("\n");
-
-
 	}
     }
 
